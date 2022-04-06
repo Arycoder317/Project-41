68b10ee1746fa73669485f531c2cef791dc7f6f8
Arycoder317 <97815172+Arycoder317@users.noreply.github.com>
2022-04-05 20:06:21 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,117 +1,59 @@
-var path,boy,bone,spike;
-var pathImg,boyImg,boneImg,spikeImg;
-var treasureCollection = 0;
-var boneG,spikeGroup;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-//Game States
-var PLAY=1;
-var END=0;
-var gameState=1;
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var baseimage;
 
-function preload(){
-  pathImg = loadImage("Road.png");
-  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
-  boneImg = loadImage("asdfgh.png");
-  spikeImg = loadImage("images.png");
-  endImg =loadAnimation("gameOver.png");
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+  baseimage = loadImage("./assets/base.png");
+  playerimage = loadImage("./assets/player.png");
 }
 
-function setup(){
-  
-  createCanvas(400,600);
-// Moving background
-path=createSprite(200,200);
-path.addImage(pathImg);
-path.velocityY = 4;
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
 
+  engine = Engine.create();
+  world = engine.world;
+  angleMode(DEGREES);
 
-//creating boy running
-boy = createSprite(70,580,20,20);
-boy.addAnimation("SahilRunning",boyImg);
-boy.scale=0.08;
-  
-  
-boneG=new Group();
-spikeGroup=new Group();
+  var options = {
+    isStatic: true
+  };
 
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
 
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player)
+
+ // playerArcher = new ( 340, playerBase.position.y - 112, 120, 120);
+ playerArcher = new PlayerArcher( 340, playerBase.position.y - 112, 120, 120);
+ 
+ 
 
 }
 
 function draw() {
+  background(backgroundImg);
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
 
-  if(gameState===PLAY){
-  background(0);
-  boy.x = World.mouseX;
-  
-  edges= createEdgeSprites();
-  boy.collide(edges);
-  
-  //code to reset the background
-  if(path.y > 400 ){
-    path.y = height/2;
-  }
-  
-    createBone();
-    createSpike();
-
-    if (boneG.isTouching(boy)) {
-      boneG.destroyEach();
-      treasureCollection=treasureCollection+50;
-    }
-      
-  
-      if(spikeGroup.isTouching(boy)) {
-        gameState=END;
-        
-
-        boy.addAnimation("SahilRunning",endImg);
-
-        boy.x=200;
-        boy.y=300;
-        boy.scale=0.6;
-        
-       
-        
-         boneG.destroyEach();
-         spikeGroup.destroyEach();
-        
-
-        
-        boneG.setVelocityYEach(0);
-        spikeGroup.setVelocityYEach(0);
-     
-    }
-  }
-  
-  drawSprites();
-  textSize(20);
-  fill(255);
-  text("Treasure: "+ treasureCollection,10,30);
-  }
+  Engine.update(engine);
 
 
-function createBone() {
-  if (World.frameCount % 200 == 0) {
-  var cash = createSprite(Math.round(random(50, 350),40, 10, 10));
-  bone.addImage(boneImg);
-  bone.scale=0.0012;
-  bone.velocityY = 3;
-  bone.lifetime = 150;
-  boneG.add(bone);
-  }
-}
+  // playerArcher.display;
 
+   playerArcher.display();
+  // display();
 
-
-
-function createSpike(){
-  if (World.frameCount % 530 == 0) {
-  var spike = createSprite(Math.round(random(50, 350),40, 10, 10));
-  spike.addImage(swordImg);
-  spike.scale=0.1;
-  spike.velocityY = 3;
-  spike.lifetime = 150;
-  spikeGroup.add(spike);
-  }
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
 }
