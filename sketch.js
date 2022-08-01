1d7795f6b64fbe07350cf6b92cfa177c08b4d97d
Arycoder317 <97815172+Arycoder317@users.noreply.github.com>
2022-08-01 18:59:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,169 +1,87 @@
 const Engine = Matter.Engine;
-const Render = Matter.Render;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
 const Body = Matter.Body;
-const Composites = Matter.Composites;
-const Composite = Matter.Composite;
 
 
-let engine;
-let world;
-var plank;
-var ground;
-var higherground;
-var con;
-var con2;
-var rope;
-var bubble,bubble_img;
+var ball
+var ball_img
+var cup
+var cup_img
+var glove
+var glove_img
+var backgroundimg
 
-function preload()
-{
-  bubble_img = loadImage("bubble.png")
-  bg_img = loadImage('background.png');
-  food = loadImage('melon.png');
-  rabbit = loadImage('Rabbit-01.png');
 
-  blink = loadAnimation("blink_1.png","blink_2.png","blink_3.png");
-  eat = loadAnimation("eat_0.png" , "eat_1.png","eat_2.png","eat_3.png","eat_4.png");
-  sad = loadAnimation("sad_1.png","sad_2.png","sad_3.png");
-  star_img = loadImage('star.png');
-  
-  blink.playing = true;
-  eat.playing = true;
-  sad.playing = true;
-  sad.looping= false;
-  eat.looping = false; 
+
+
+
+
+
+function preload() {
+  ball_img = loadImage("ball.png")
+  cup_img = loadImage("cup.png")
+  glove_img = loadImage("glove.png")
+  backgroundimg = loadImage("background.png")
 }
 
 function setup() {
-  createCanvas(500,800);
+  createCanvas(800,400);
   frameRate(80);
   engine = Engine.create();
   world = engine.world;
 
-   var fruit_options = {
-    restitution: 0.8
+
+
+
+  var ball_options = {
+    restitution:0.95,
+    frictionAir:0.01
   }
-  
-  ground =new Ground(250,height-10,width,20);
-  fruit = Bodies.circle(100,400,15,fruit_options);
-  World.add(world,fruit);
-  
-  bubble = createSprite(290,460,20,20);
-  bubble.addImage(bubble_img);
-  bubble.scale = 0.1;
-  
-  //bunny sprite
-  blink.frameDelay = 20;
-  eat.frameDelay = 20;
-  bunny = createSprite(270,100,100,100);
-  bunny.addImage(rabbit);
-  bunny.scale = 0.2;
-  higherground =new Ground(300,170,100,10);
 
-  bunny.addAnimation('blinking',blink);
-  bunny.addAnimation('eating',eat);
-  bunny.addAnimation('crying',sad);
-  bunny.changeAnimation('blinking');
 
-  rope = new Rope(5,{x:230,y:330});
-  rope2 = new Rope(4,{x:50,y:450});
-  con = new Link(rope,fruit);
-  con2 = new Link(rope2,fruit);
 
-  //btn 1
-  button = createImg('cut_btn.png');
-  button.position(200,320);
-  button.size(50,50);
+  ball = Bodies.circle(150,60,100, ball_options)
+  World.add(world, ball);
 
-  button2 = createImg('cut_btn.png');
-  button2.position(30,420);
-  button2.size(50,50);
+  glove = Bodies.rectangle(300,100,200,200);
+  World.add(world, glove);
+  glove.mousePressed(hForce);
 
-  //button2.Clicked(drop);
-  
-  //button2.mousePress(drop);
-  
-  //button2.mouseClick(drop);
+  cup = Bodies.rectangle(300,380,200,200);
+  World.add(world, cup);
 
-  button2.mouseClicked(drop);
 
-  ellipseMode(RADIUS);
+
 }
+
+
+
 
 function draw() 
 {
   background(51);
-  image(bg_img,0,0,width,height);
+  image(backgroundimg, 0,0, displayWidth+80, displayHeight)
   Engine.update(engine);
-  
-  push();
+
   imageMode(CENTER);
-  if(fruit!=null){
-    image(food,fruit.position.x,fruit.position.y,70,70);
-  }
-  pop();
+  image(ball_img, ball.position.x, ball.position.y, 100, 100);
+  image(glove_img, glove.position.x, glove.position.y, 200, 200);
+  image(cup_img, cup.position.x, cup.position.y, 200, 200);
 
-  ground.show();
-  higherground.show();
-  rope.show();
-  rope2.show();
+  collide()
+  hForce()
+  mousePressed()
 
-  if(collide(fruit,bunny,80)==true)
-  {
-   remove_rope();
-   bubble.visible = false;
-    World.remove(engine.world,fruit);
-    fruit = null;
-    //bunny.change('eating');
-
-    bunny.changeAnimation('eating');
-
-    //bunny.changeAnimation();
-
-    //bunny.Animation('eating');
-  }
-  
-  if(collide(fruit,bubble,40) == true)
-    {
-      engine.world.gravity.y = -1;
-      bubble.position.x = fruit.position.x;
-      bubble.position.y = fruit.position.y;
-    }
-
-  drawSprites();
-
+  drawSprites()
 }
 
-function drop()
+function collide(cup, ball)
 {
-  rope2.break();
-  con2.dettach();
-  con2 = null; 
+  World.remove(engine.world,ball, cup);
 }
 
-function remove_rope()
+function hForce()
 {
-  rope.break();
-  con.dettach();
-  con = null; 
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
 }
-
-function collide(body,sprite,x)
-{
-  if(body!=null)
-        {
-         var d = dist(body.position.x,body.position.y,sprite.position.x,sprite.position.y);
-          if(d<=x)
-            {
-              
-               return true; 
-            }
-            else{
-              return false;
-            }
-         }
-}
-
