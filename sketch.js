f0e49d9e238bf7634d5104bf8160462318362572
Arycoder317 <97815172+Arycoder317@users.noreply.github.com>
2022-08-18 18:15:45 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,80 +1,30 @@
-var PLAY = 1;
-var END = 0;
-var gameState = PLAY;
+var canvas, backgroundImage;
 
-var dog,dogImage
-var ground, invisibleGround, groundImage;
-var bone, boneImage
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
 
-var obstacle, obstaclesGroup
+var question, contestant, quiz;
 
-function preload(){
-  
-  dogImage = loadImage("Dog.png");
-  groundImage = loadImage("Path.png");
-  boneImage = loadImage("Bone.png")
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
 }
 
-function setup() {
-  createCanvas(600, 200);
-  
-  dog = createSprite(50,180,20,50);
-  dog.addImage("dog", dogImage)
-  dog.scale = 0.5;
-  
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
 
-  bone = createSprite(200,150,20,50);
-  bone.addImage("bone", boneImage);
-  bone.scale = 0.5;
-  
-  
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
-  
-
-
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
 }
-
-function draw() {
-  background(180);
-  
-  
-  
-  if(gameState === PLAY){
-    //move the ground
-    ground.velocityX = -4;
-    //scoring
-  
-    
-    
-
-    if (ground.x < 0){
-      ground.x = ground.width/2;
-    }
-    
-    //jump when the space key is pressed
-    if(keyDown("space")&& dog.y >=100) {
-        dog.velocityY = -13;
-
-        
-    }
-    
-    //add gravity
-    dog.velocityY = dog.velocityY + 0.8
-  
-    
-   
-  
- 
-  //stop trex from falling down
-  dog.collide(invisibleGround);
-  
-  
-  
-  drawSprites();
-}
-}
-
