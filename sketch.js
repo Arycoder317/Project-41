817c0acdb103fff4e66cfb5dada38ad3fff45dc9
Arycoder317 <97815172+Arycoder317@users.noreply.github.com>
2022-09-01 19:35:43 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,36 @@
-var canvas, backgroundImage;
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2;
+var cars = [];
 
-var gameState = 0;
-var contestantCount;
-var allContestants;
-var answer;
-var database;
-
-var question, contestant, quiz;
-
-
-function setup(){
-  canvas = createCanvas(850,400);
-  database = firebase.database();
-  quiz = new Quiz();
-  quiz.getState();
-  quiz.start();
+function preload() {
+  backgroundImage = loadImage("assets/background.png");
+  car1_img = loadImage("assets/car1.png");
+  car2_img = loadImage("assets/car2.png");
+  track = loadImage("assets/track.jpg");
 }
 
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
 
-function draw(){
-  background("pink");
-  if(contestantCount === 2){
-    quiz.update(1);
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
   }
-  if(gameState === 1){
-    clear();
-    quiz.play();
+
+  if (gameState === 1) {
+    game.play();
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
